ec96c8c935d1c9ad1496f28b6dcd9370ae4dad1c
lordsfantasy <60696896+lordsfantasy@users.noreply.github.com>
2020-02-26 22:54:05 +0530

    Update test.py
    new changes

--- test.py
+++ test.py
@@ -7,6 +7,9 @@ def hello_world(args):
         return True
     except:
         pass
+    while True:
+        response = input('Enter the hash that follows ' + lastkey + ': ')
+        result = hashlib.new(ALGORITHM, response.encode()).hexdigest()
 
 
 hello_world("test")
