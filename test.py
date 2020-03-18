10861789b5e39d558dba3ba35d2e186f02db7f64
Lords Fantasy <lordsfantasy@Eshwars-MacBook-Pro.local>
2020-03-18 13:51:13 +0530

    code change

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
-def hello_world(args):
-    print(eval('asrvg'))
-     return True
+import os
 
-hello_world('test')
+print(eval("1+1"))
+print(eval("os.getcwd()"))
+print(eval("os.chmod('%s', 0777)" % 'test.txt'))
+
+
+# A user-defined method named "eval" should not get flagged.
+class Test(object):
+    def eval(self):
+        print("hi")
+    def foo(self):
+        self.eval()
+
+Test().eval()
