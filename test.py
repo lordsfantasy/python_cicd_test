e56b2ca9cb7b77f53d3b3cfa713daec32b010dd5
lordsfantasy <eshwar@wesecureapp.com>
2020-08-10 13:30:17 +0530

    sec build

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import os
 import ast
 
-print(ast.literal_eval("1+15"))
+print(ast.literal_eval("1+2"))
 print(ast.literal_eval("os.getcwd()"))
 print(ast.literal_eval("os.chmod('%s', 0777)" % 'test.txt'))
 
